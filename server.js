0dab7f95039d502c74b3800e7ad3d93025728df3
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-09-30 18:06:04 -0600

    Complete CRUD functionality

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT;
 const Animal = require('./models/animals.js');
 const { application } = require('express');
 
-
-
 //MIDDLEWARE
 ///////////////////////////
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +21,7 @@ app.use(methodOverride("_method"));
 //////////////////////////
 //HOME -get
 app.get('/', (req, res) => {
-    res.send(`HOMEPAGE`)
+    res.send(`HOMEPAGE`);
 });
 //SEED -get
 app.get('/animals/seed', (req, res) => {
@@ -41,37 +39,49 @@ app.get('/animals/seed', (req, res) => {
 //INDEX -get
 app.get('/animals', (req, res) => {
     Animal.find({}, (err, animals) => {
-        res.render('animals/index.ejs', { animals })
+        res.render('animals/index.ejs', { animals });
     });
 });
 //NEW -get
 app.get('/animals/new', (req, res) => {
     res.render('animals/new.ejs');
 });
-
 //DESTROY -delete
-
+app.delete('/animals/:id', (req, res) => {
+    const { id } = req.params;
+    Animal.findByIdAndDelete(id, (err, animal) => {
+        res.redirect('/animals');
+    });
+});
 //UPDATE - put
-
+app.put('/animals/:id', (req, res) => {
+    const { id } = req.params;
+    req.body.extinct = req.body.extinct === "on" ? true: false
+    Animal.findByIdAndUpdate(id, req.body, {new:true}, (err, animal) => {
+        res.redirect('/animals');
+    });
+});
 //CREATE - post
 app.post('/animals', (req, res) => {
     req.body.extinct = req.body.extinct === "on" ? true : false
     Animal.create(req.body, (err, animal) => {
         res.redirect('/animals');
-    })
-    
-})
-
-//EDIT - get
-
-//SHOW - get
-app.get('/animals/:id', (req, res) => {
-    const {id} = req.params
-    Animal.findById(id, (err, animal) => {
-        res.render('animals/show.ejs', { animal })
     });
 });
-
+//EDIT - get
+app.get('/animals/:id/edit', (req, res) => {
+    const { id } = req.params;
+    Animal.findById(id, (err, animal) => {
+        res.render('animals/edit.ejs', { animal });
+    });
+});
+//SHOW - get
+app.get('/animals/:id', (req, res) => {
+    const { id } = req.params;
+    Animal.findById(id, (err, animal) => {
+        res.render('animals/show.ejs', { animal });
+    });
+});
 //////////////////////
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
