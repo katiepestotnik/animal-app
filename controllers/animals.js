af339fe6d21d406dff574fe3bddabede1972e384
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-10-01 19:20:18 -0600

    Complete up to controller refactor

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,9 +1,65 @@
 const express = require('express');
-const Animals = require('../models/animals.js');
+const Animal = require('../models/animals.js');
 
 const router = express.Router();
 
-
-
-
+//SEED -get
+router.get('/seed', (req, res) => {
+    const startAnimals = [
+        { species: "Dumbo Octopus", extinct: false, location: ">= 13100 feet under the sea", lifeExpectancy: 5 },
+        { species: "Tarsier", extinct: false, location: "Southeast Asia", lifeExpectancy: 12 },
+        { species: "Tasmanian Tiger", extinct: true, location: "Tasmania, Australia, New Guinea", lifeExpectancy: 5 }
+    ]
+    Animal.deleteMany({}, (err, data) => {
+        Animal.create(startAnimals, (err, data) => {
+            res.json(data);
+        });
+    });
+});
+//INDEX -get
+router.get('/', (req, res) => {
+    Animal.find({}, (err, animals) => {
+        res.render('animals/index.ejs', { animals });
+    });
+});
+//NEW -get
+router.get('/new', (req, res) => {
+    res.render('animals/new.ejs');
+});
+//DESTROY -delete
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Animal.findByIdAndDelete(id, (err, animal) => {
+        res.redirect('/animals');
+    });
+});
+//UPDATE - put
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    req.body.extinct = req.body.extinct === "on" ? true: false
+    Animal.findByIdAndUpdate(id, req.body, {new:true}, (err, animal) => {
+        res.redirect('/animals');
+    });
+});
+//CREATE - post
+router.post('/', (req, res) => {
+    req.body.extinct = req.body.extinct === "on" ? true : false
+    Animal.create(req.body, (err, animal) => {
+        res.redirect('/animals');
+    });
+});
+//EDIT - get
+router.get('/:id/edit', (req, res) => {
+    const { id } = req.params;
+    Animal.findById(id, (err, animal) => {
+        res.render('animals/edit.ejs', { animal });
+    });
+});
+//SHOW - get
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Animal.findById(id, (err, animal) => {
+        res.render('animals/show.ejs', { animal });
+    });
+});
 module.exports = router
--- server.js
+++ server.js
@@ -7,80 +7,20 @@ const app = express();
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const PORT = process.env.PORT;
-const Animal = require('./models/animals.js');
-const { application } = require('express');
-
+const AnimalRouter = require('./controllers/animals');
 //MIDDLEWARE
 ///////////////////////////
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 app.use(express.static('public'));
 app.use(methodOverride("_method"));
+app.use('/animals', AnimalRouter);
 //////////////////////////
 //ROUTES
 //////////////////////////
 //HOME -get
 app.get('/', (req, res) => {
     res.send(`HOMEPAGE`);
-});
-//SEED -get
-app.get('/animals/seed', (req, res) => {
-    const startAnimals = [
-        { species: "Dumbo Octopus", extinct: false, location: ">= 13100 feet under the sea", lifeExpectancy: 5 },
-        { species: "Tarsier", extinct: false, location: "Southeast Asia", lifeExpectancy: 12 },
-        { species: "Tasmanian Tiger", extinct: true, location: "Tasmania, Australia, New Guinea", lifeExpectancy: 5 }
-    ]
-    Animal.deleteMany({}, (err, data) => {
-        Animal.create(startAnimals, (err, data) => {
-            res.json(data);
-        });
-    });
-});
-//INDEX -get
-app.get('/animals', (req, res) => {
-    Animal.find({}, (err, animals) => {
-        res.render('animals/index.ejs', { animals });
-    });
-});
-//NEW -get
-app.get('/animals/new', (req, res) => {
-    res.render('animals/new.ejs');
-});
-//DESTROY -delete
-app.delete('/animals/:id', (req, res) => {
-    const { id } = req.params;
-    Animal.findByIdAndDelete(id, (err, animal) => {
-        res.redirect('/animals');
-    });
-});
-//UPDATE - put
-app.put('/animals/:id', (req, res) => {
-    const { id } = req.params;
-    req.body.extinct = req.body.extinct === "on" ? true: false
-    Animal.findByIdAndUpdate(id, req.body, {new:true}, (err, animal) => {
-        res.redirect('/animals');
-    });
-});
-//CREATE - post
-app.post('/animals', (req, res) => {
-    req.body.extinct = req.body.extinct === "on" ? true : false
-    Animal.create(req.body, (err, animal) => {
-        res.redirect('/animals');
-    });
-});
-//EDIT - get
-app.get('/animals/:id/edit', (req, res) => {
-    const { id } = req.params;
-    Animal.findById(id, (err, animal) => {
-        res.render('animals/edit.ejs', { animal });
-    });
-});
-//SHOW - get
-app.get('/animals/:id', (req, res) => {
-    const { id } = req.params;
-    Animal.findById(id, (err, animal) => {
-        res.render('animals/show.ejs', { animal });
-    });
 });
 //////////////////////
 app.listen(PORT, () => {
