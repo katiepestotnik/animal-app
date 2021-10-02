88d3f8cb7ecae855c8343e9ac35dc50a93617c2d
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-10-02 15:26:36 -0600

    Add user route, ejs, middleware, model

---
+++ controllers/user.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+const router = express.Router();
+//signup
+router.get('/signup', (req, res) => {
+    res.render('user/signup.ejs')
+});
+
+router.post('/signup', async (req, res) => {
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10));
+    User.create(req.body, (err, user) => {
+        res.redirect('/user/login');
+    });
+});
+//login
+router.get('/login', (req, res) => {
+    res.render('user/login.ejs');
+});
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    User.findOne({ username }, (err, user) => {
+        console.log(err, user);
+        if (!user) {
+            res.send('USER DOES NOT EXIST');
+        } else {
+            const result = bcrypt.compareSync(password, user.password);
+            if (result) {
+                req.session.username = username;
+                req.session.loggedIn = true;
+                res.redirect('/animals');
+            } else {
+                res.send('Incorrect Password')
+            };
+        };
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,13 +8,25 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const PORT = process.env.PORT;
 const AnimalRouter = require('./controllers/animals');
+const UserRouter = require('./controllers/user');
+const HomeRouter = require('./controllers/home')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 //MIDDLEWARE
 ///////////////////////////
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 app.use(express.static('public'));
 app.use(methodOverride("_method"));
+app.use(session({
+    secret: process.env.SECRET,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URL }),
+    saveUninitialized: true,
+    resave: false
+}))
 app.use('/animals', AnimalRouter);
+app.use('/user', UserRouter);
+app.use('/', HomeRouter);
 //////////////////////////
 //ROUTES
 //////////////////////////
